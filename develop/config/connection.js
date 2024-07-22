dc7f739af2339846cafe59b6c1c3ebd5b1d47191
KatLeviathan <alexisbrown-renshaw@outlook.com>
2024-07-22 19:09:54 -0400

    reverting last commit

--- develop/config/connection.js
+++ develop/config/connection.js
@@ -6,8 +6,9 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: '127.0.0.1',
+    host: process.env.DB_HOST,
     dialect: 'postgres',
+    port: process.env.DB_PORT,
   }
 );
 
