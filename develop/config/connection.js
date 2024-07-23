4710e47ad0a792ace0cfa729cbc95a7da2223971
KatLeviathan <alexisbrown-renshaw@outlook.com>
2024-07-22 21:01:39 -0400

    commit

--- develop/config/connection.js
+++ develop/config/connection.js
@@ -4,7 +4,8 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.DB_URL) {
-  
+  sequelize = new Sequelize(process.env.DB_URL);
+} else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
